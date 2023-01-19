a545ffaab1d49c8c286119b886b9fe2d74db475e
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-01-19 22:44:17 +0900

    フレームレートによる制御を追加

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -4,6 +4,12 @@
 #include "Game.h"
 
 int main(int argc, char* args[]) {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	Game* game = nullptr;
 
 	game = new Game();
@@ -11,9 +17,17 @@ int main(int argc, char* args[]) {
 	game->Init("SDL Sample", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game->IsRunning()) {
+		frameStart = SDL_GetTicks();
+
 		game->HandleEvnets();
 		game->Update();
 		game->Render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->Clean();
